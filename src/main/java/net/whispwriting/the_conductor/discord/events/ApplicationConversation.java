388709f13170026f45bb8fe7252ee4db17097131
DSlyDog <drew.sly@verizon.net>
2024-08-04 21:37:38 -0400

    - fixed bug where pressing the yes/no/done buttons in a previous message in the application conversation would cause the bot to skip things

--- src/main/java/net/whispwriting/the_conductor/discord/events/ApplicationConversation.java
+++ src/main/java/net/whispwriting/the_conductor/discord/events/ApplicationConversation.java
@@ -7,6 +7,8 @@ import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.ItemComponent;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle;
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
@@ -117,6 +119,8 @@ public class ApplicationConversation extends ListenerAdapter {
             if (!event.getButton().getId().contains(this.ticketID + "")){
                 return;
             }
+
+            event.getMessage().editMessageComponents().setComponents().queue();
 
             event.deferEdit().queue();
 
